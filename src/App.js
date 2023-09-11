6ec0e487935956a6a018fff082d4d194a4fc5f90
Anas Bin Ateeq <anasbinateeq15@gmail.com>
2023-09-11 12:15:27 +0500

    added Context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Menu from './components/Menu';
-import WelcomePage from './pages/WelcomePage';
-import WeatherPage from './pages/WeatherPage';
-import ClockPage from './pages/ClockPage';
-import GithubPage from './pages/GithubPage';
-import DeveloperPage from './pages/DeveloperPage';
-import AboutPage from './pages/AboutPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import WelcomePage from "./pages/WelcomePage";
+import WeatherPage from "./pages/WeatherPage";
+import ClockPage from "./pages/ClockPage";
+import DeveloperPage from "./pages/DeveloperPage";
+import AboutPage from "./pages/AboutPage";
+import React, { useState} from "react";
+import axios from "axios";
+import AppContext from "./components/AppContext";
 
 function App() {
+  const [ipInfo, setIpInfo] = useState({
+    ip: "Loading ...",
+    city: "Loading ...",
+    country: "Loading ...",
+  });
+
+  const fetchIpAddress = async () => {
+    await axios
+      .get("http://ip-api.com/json/")
+      .then((response) => {
+        console.log(response.data.query);
+        const { query, city, country } = response.data;
+        const ipInfoObject = { ip: query, city, country };
+        setIpInfo(ipInfoObject);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const globalVariables = {
+    ipInformation : ipInfo,
+    fetchIpAddress,
+  };
   return (
+    <AppContext.Provider value={globalVariables}>
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" exact element={<WelcomePage/>} />
-          <Route path="/weather" element={<WeatherPage/>} />
-          <Route path="/clock" element={<ClockPage/>} />
-          <Route path="/developer" element={<DeveloperPage/>} />
-          <Route path="/aboutus" element={<AboutPage/>} />
+          <Route path="/" exact element={<WelcomePage />} />
+          <Route path="/weather" element={<WeatherPage />} />
+          <Route path="/clock" element={<ClockPage />} />
+          <Route path="/developer" element={<DeveloperPage />} />
+          <Route path="/aboutus" element={<AboutPage />} />
         </Routes>
-    </Router>
+      </Router>
     </div>
+    </AppContext.Provider>
   );
 }
 
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,51 +1,54 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useContext } from "react";
 import "../styles/Welcome.css";
 import building from "../assets/buildings.png";
 import axios from "axios";
+import { Button } from "reactstrap";
+import AppContext from "../components/AppContext";
+import {useNavigate} from 'react-router-dom';
 
 function WelcomePage() {
-  const [ipInfo, setIpInfo] = useState({
-    ip:"Loading ...",
-    city:"Loading ...",
-    country:"Loading ..."
-  });
-
-  const fetchIpAddress = async () => {
-    await axios
-      .get("http://ip-api.com/json/")
-      .then(
-        response => {
-            console.log(response.data.query);
-            const { query, city, country } = response.data;
-            const ipInfoObject = { ip: query, city, country };
-            setIpInfo(ipInfoObject);
-        })
-      .catch((err) => console.log(err));
-  };
+  const [ipLoaded, setIpLoaded] = useState(false)
+  const WelcomeContext = useContext(AppContext)
 
   useEffect(() => {
-    fetchIpAddress(); // Fetch IP address when the component mounts
+    WelcomeContext.fetchIpAddress(); // Fetch IP address when the component mounts
+    setIpLoaded(true);
   }, []);
+
+  const navigate = useNavigate();
+
+  const Continue = () => {
+    if(ipLoaded){
+      navigate("/weather")
+    }
+  }
 
   return (
     <div className="Container">
       <div className="Row d-flex justify-content-center align-items-center WelcomeContent">
         <div className="col-8 text-center WelcomeColumn">
           <img src={building} className="Building" />
-          <div className="Title">
+          <div className="Title d-flex justify-content-center mb-0">
             <div className="Weather">Weather</div>
             <div className="Clock">
               <span>& Clock</span>
             </div>
           </div>
-          <div className="WelcomeLocation m-3 d-flex justify-content-between">
-            <div>IP Address: <span className="ipDetail">{ipInfo.ip}</span></div>
-            <div>City: <span className="ipDetail">{ipInfo.city}</span></div>
-            <div>Country: <span className="ipDetail">{ipInfo.country}</span></div>
+          <div className="WelcomeLocation d-flex justify-content-around align-items-center m-auto">
+            <div>
+              IP Address: <span className="ipDetail">{WelcomeContext.ipInformation.ip}</span>
+            </div>
+            <div>
+              City: <span className="ipDetail">{WelcomeContext.ipInformation.city}</span>
+            </div>
+            <div>
+              Country: <span className="ipDetail">{WelcomeContext.ipInformation.country}</span>
+            </div>
           </div>
-          <a className="Continue" href="/weather">
+          <div>
+          <Button className="Continue m-3" onClick={Continue}>
             Continue
-          </a>
+          </Button></div>
         </div>
       </div>
     </div>
